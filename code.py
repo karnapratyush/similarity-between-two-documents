3e0ce62350bd0e099df01e865db1f763273c03d7
karnapratyush <45131300+karnapratyush@users.noreply.github.com>
2019-08-28 18:48:11 +0530

    Update and rename code to code.py

--- code.py
+++ code.py
@@ -1,8 +1,12 @@
+# pasting the documents from the web
+
 sen_1=''' '''
 sen_2=''' '''
 
-
+# the k array will have unique words
 k=[]
+# here I am defining a function 'arr' which will first convert the strings of para_1 and 2 into words. then the unique words are stored in k while all words of sen_1 will be stored in 'a' 
+
 def arr(sen,a):
     exclude=[',',' ', ':', ';' , '?','"','-', '(', ')']
     if len(sen)==1:
@@ -31,30 +35,36 @@ li_2=[]
 arr(sen_1,li_1)
 arr(sen_2,li_2)
 
-
+# making a dictionary from k and making its copy  for sen_2
 d_1={}
 for i in k:
     d_1[i]=0
 d_2=d_1.copy()
 
-
+# counting the times the words are repeated in sen_1 i.e. calculating magnitude of words vectors in sen_1 and sen_2
 for i in li_1:
     d_1[i]=+1
     
 for i in li_2:
     d_2[i]=+1
 
+# calculating u.v
 
 dot=0
 for i in k:
     dot=dot+d_1[i]*d_2[i]
 
+
+#calculating sqrt of u and v
 def sq_rt(d):
     sum=0
     for value in d.values():
         sum=sum+value**2
     sum=sum**0.5
     return sum
+ 
+ 
+ #finding theta
 
 theta=(dot/(sq_rt(d_1)*sq_rt(d_2)))*100
 print('the similarity is', theta)
